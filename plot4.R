f5aecacbd41fad4c2ad2170b3863ec43504db1ab
Andrew Curtis <andrew.curtis@ll.mit.edu>
2015-04-12 15:11:10 -0400

    Added plot 4

--- plot4.R
+++ plot4.R
@@ -22,3 +22,71 @@ data <- data[complete.cases(data),]
 
 
 ## Plot 4
+
+# Save to png file
+png(filename = "plot4.png", width=480, height=480)
+
+# Set up 2x2 plotting
+par(mfrow=c(2,2))
+
+# Top left plot (Global Active Power)
+
+# Create the plot and labels, but no lines
+with(data, plot(Date, Global_active_power, 
+                xlab="",
+                ylab="Global Active Power",
+                type="n"))
+
+# Add lines to plot
+with(data, lines(Date, Global_active_power),
+     col="black")
+
+# Top right plot (Voltage)
+
+# Create the plot and labels, but no lines
+with(data, plot(Date, Voltage, 
+                xlab="datetime",
+                ylab="Voltage",
+                type="n"))
+
+# Add line to plot
+with(data, lines(Date, Voltage),
+     col="black")
+
+# Bottom left plot (Energy Sub Metering)
+
+# Create the plot and labels, no lines
+with(data, plot(Date, Sub_metering_1, 
+                xlab="",
+                ylab="Energy sub metering",
+                type="n"))
+
+# Add lines for the three different measurements
+with(data, lines(Date, Sub_metering_1,
+                 col="black"))
+with(data, lines(Date, Sub_metering_2,
+                 col="red"))
+with(data, lines(Date, Sub_metering_3,
+                 col="blue"))
+
+# Add a legend (without a box) to describe the lines
+legend("topright",
+       lty=1,
+       bty="n",
+       col=c("black","red","blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# Bottom right plot (Global Reactive Power)
+
+# Create the plot and labels, but no lines
+with(data, plot(Date, Global_reactive_power, 
+                xlab="datetime",
+                ylab="Global_reactive_power",
+                type="n"))
+
+# Add line to plot
+with(data, lines(Date, Global_reactive_power),
+     col="black")
+
+# End plotting
+dev.off()
